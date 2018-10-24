6a96d7adb155d0c54d1c9a2be2ea5024d0f26387
Changmin Choi <cmchoi9901@gmail.com>
2018-10-24 23:22:05 +0900

    hw4 ex3 done..?

--- hw4/ex3.ml
+++ hw4/ex3.ml
@@ -29,7 +29,7 @@ let rec com: gift list -> gift list -> gift list -> gift list = fun re a b -> (
     match b with
     | [] -> re
     | _ -> (
-      if (List.hd a = List.hd b) then (com (re::(List.hd a)::[]) (List.tl a) (List.tl b)) else (
+      if (List.hd a = List.hd b) then (com (re @ (List.hd a)::[]) (List.tl a) (List.tl b)) else (
         if (List.hd a > List.hd b) then (com re a (List.tl b)) else (com re (List.tl a) b)
       )
     )
@@ -37,23 +37,23 @@ let rec com: gift list -> gift list -> gift list -> gift list = fun re a b -> (
 )
 
 let conc: gift list -> gift list -> gift list = fun a b -> (
-  let fx l x = if (List.mem l x) then (l) else (l::x::[]) in
+  let fx: gift list -> gift -> gift list = fun l x -> (if (List.mem x l) then (l) else (l @ x::[])) in
   List.fold_left fx a b
 )
 
 let rec eval: cond -> gift list = fun c -> (
-  match cond with
+  match c with
   | Items gl -> gl
   | Same i -> Hashtbl.find allL i
   | Common (a, b) -> (
-    let al: gift list = List.sort List.compare (eval a) in
-    let bl: gift list = List.sort List.compare (eval b) in
-    com al bl
+    let al: gift list = List.sort compare (eval a) in
+    let bl: gift list = List.sort compare (eval b) in
+    com [] al bl
   )
   | Except (a, b) -> (
     let al: gift list = eval a in
     let re: gift list = [] in
-    let fx: gift list -> gift -> gift list = fun ret aa -> if(not (List.mem aa b)) then (ret::aa::[]) else (ret) in
+    let fx: gift list -> gift -> gift list = fun ret aa -> if(not (List.mem aa b)) then (ret @ aa::[]) else (ret) in
     let rrr = List.fold_left fx re al in
     rrr
   )
@@ -65,11 +65,22 @@ let rec evalR: cond list -> gift list -> gift list = fun conL re -> (
   )
 )
 
-let rec shop: require list -> (id, int) Hashtbl.t = fun req -> (
+let rec shopL: require list -> unit = fun req -> (
   let reD = ref 0 in
-  let _ = Hashtbl.replace allL (fst (List.nth req 1)) (evalR (snd (List.nth req 1))) in
+
+  let shp: require -> unit = fun reqqq -> (
+    let _ = Hashtbl.replace allL (fst reqqq) (evalR (snd reqqq) []) in
+    let _ = reD := if (Hashtbl.find cAll (fst reqqq) = (List.length (Hashtbl.find allL (fst reqqq)))) then (!reD) else (1) in
+    let _ = Hashtbl.replace cAll (fst reqqq) (List.length (Hashtbl.find allL (fst reqqq))) in
+    ()
+  ) in
+
+  let _ = List.iter shp req in
+  let r = if(!reD = 0) then () else (shopL req) in
+  r
 )
 
 let shoppingList: require list -> (id * gift list) list = fun req -> (
-
+  let _ = shopL req in
+  Hashtbl.fold (fun k v l -> (k, v)::l) allL []
 )
