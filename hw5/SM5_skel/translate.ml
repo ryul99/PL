09d28e12a1deb2dbc344c41d37c320a902783bcc
Changmin Choi <cmchoi9901@gmail.com>
2018-11-16 18:48:34 +0900

    hw5-1 done

--- hw5/SM5_skel/translate.ml
+++ hw5/SM5_skel/translate.ml
@@ -29,9 +29,9 @@ module Translator = struct
     | K.SEQ (ex1, ex2) -> trans ex1 @ [Sm5.POP] @ trans ex2 (*SEQ doesnt change env?*)
     | K.IF (ex, ex1, ex2) -> trans ex @ [Sm5.JTR (trans ex1, trans ex2)]
     | K.WHILE (e1, e2) -> (
-      [Sm5.PUSH (Sm5.Fn("$", [Sm5.BIND "@"] @ trans e1 @ [Sm5.JTR(trans e2 @ [Sm5.PUSH (Sm5.Id "@");Sm5.PUSH (Sm5.Id "@");
+      [Sm5.PUSH (Sm5.Fn("$", [Sm5.BIND "@"] @ trans e1 @ [Sm5.JTR(trans e2 @ [Sm5.POP; Sm5.PUSH (Sm5.Id "@");Sm5.PUSH (Sm5.Id "@");
       Sm5.PUSH (Sm5.Val (Sm5.B true)); Sm5.PUSH (Sm5.Id "$"); Sm5.CALL], [Sm5.PUSH (Sm5.Val (Sm5.Unit))]); Sm5.UNBIND; Sm5.POP])); Sm5.BIND "@"] @ [Sm5.PUSH (Sm5.Id "@"); Sm5.PUSH (Sm5.Id "@");
-      Sm5.PUSH (Sm5.Val (Sm5.B true)); Sm5.MALLOC; Sm5.CALL]
+      Sm5.PUSH (Sm5.Val (Sm5.B true)); Sm5.MALLOC; Sm5.CALL; Sm5.UNBIND; Sm5.POP]
     )
     | K.FOR (id, ex1, ex2, ex3) -> (
       (* trans ex1 @ [Sm5.MALLOC; Sm5.BIND "!"; Sm5.PUSH (Sm5.Id "!"); Sm5.STORE] @ trans ex2 @ [Sm5.MALLOC; Sm5.BIND "max@"; Sm5.PUSH (Sm5.Id "max@"); Sm5.STORE] @
@@ -43,8 +43,8 @@ module Translator = struct
       trans ex1 @ [Sm5.MALLOC; Sm5.BIND "!"; Sm5.PUSH (Sm5.Id "!"); Sm5.STORE] @ trans ex2 @ [Sm5.MALLOC; Sm5.BIND "max@"; Sm5.PUSH (Sm5.Id "max@"); Sm5.STORE] @
 
       [Sm5.PUSH (Sm5.Fn("F$", [Sm5.BIND "F@"] @ [Sm5.PUSH (Sm5.Id "max@"); Sm5.LOAD; Sm5.PUSH (Sm5.Id "F$"); Sm5.LOAD; Sm5.LESS; Sm5.NOT] @ [Sm5.JTR([Sm5.PUSH (Sm5.Id "F$"); Sm5.LOAD; Sm5.PUSH (Sm5.Id id); Sm5.STORE]
-      @ trans ex3 @ [Sm5.PUSH (Sm5.Id "F@");Sm5.PUSH (Sm5.Id "F@"); Sm5.PUSH (Sm5.Id "F$"); Sm5.LOAD; Sm5.PUSH (Sm5.Val (Sm5.Z 1)); Sm5.ADD; Sm5.PUSH (Sm5.Id "F$"); Sm5.CALL] @ [Sm5.PUSH (Sm5.Val (Sm5.Unit))], [Sm5.PUSH (Sm5.Val (Sm5.Unit))]);
-      Sm5.UNBIND; Sm5.POP])); Sm5.BIND "F@"] @ [Sm5.PUSH (Sm5.Id "F@"); Sm5.PUSH (Sm5.Id "F@"); Sm5.PUSH (Sm5.Id "!"); Sm5.LOAD; Sm5.MALLOC; Sm5.CALL]
+      @ trans ex3 @ [Sm5.POP; Sm5.PUSH (Sm5.Id "F@");Sm5.PUSH (Sm5.Id "F@"); Sm5.PUSH (Sm5.Id "F$"); Sm5.LOAD; Sm5.PUSH (Sm5.Val (Sm5.Z 1)); Sm5.ADD; Sm5.PUSH (Sm5.Id "F$"); Sm5.CALL], [Sm5.PUSH (Sm5.Val (Sm5.Unit)); Sm5.POP]);
+      Sm5.UNBIND; Sm5.POP])); Sm5.BIND "F@"] @ [Sm5.PUSH (Sm5.Id "F@"); Sm5.PUSH (Sm5.Id "F@"); Sm5.PUSH (Sm5.Id "!"); Sm5.LOAD; Sm5.MALLOC; Sm5.CALL; Sm5.PUSH (Sm5.Val (Sm5.Unit)); Sm5.UNBIND; Sm5.POP; Sm5.UNBIND; Sm5.POP; Sm5.UNBIND; Sm5.POP]
     )
     | K.LETF (f, x, e1, e2) -> (
       [Sm5.PUSH (Sm5.Fn(x, [Sm5.BIND f] @ trans e1 @ [Sm5.UNBIND; Sm5.POP])); Sm5.BIND f] @ trans e2 @ [Sm5.UNBIND; Sm5.POP]
