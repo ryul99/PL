23c3ac73421d90d186be29053f172e504c657b3e
Changmin Choi <cmchoi9901@gmail.com>
2018-11-17 14:07:49 +0900

    hw5-2(gc) done???

--- hw5/SM5_skel/sm5.ml
+++ hw5/SM5_skel/sm5.ml
@@ -1,30 +1,30 @@
 (*
- * SNU 4190.310 Programming Languages 
+ * SNU 4190.310 Programming Languages
  * Homework "SM5 Garbage Collector"
  *)
 
-module type SM5 = 
+module type SM5 =
 sig
   type record
-  type loc 
+  type loc
   type value = Z of int | B of bool | L of loc | Unit | R of record
-  type cmd = 
-    | PUSH of obj 
-    | POP 
-    | STORE 
-    | LOAD 
+  type cmd =
+    | PUSH of obj
+    | POP
+    | STORE
+    | LOAD
     | JTR of command * command
-    | MALLOC 
-    | BOX of int 
-    | UNBOX of string 
-    | BIND of string 
+    | MALLOC
+    | BOX of int
+    | UNBOX of string
+    | BIND of string
     | UNBIND
-    | GET 
-    | PUT 
-    | CALL 
-    | ADD 
-    | SUB 
-    | MUL 
+    | GET
+    | PUT
+    | CALL
+    | ADD
+    | SUB
+    | MUL
     | DIV
     | EQ
     | LESS
@@ -47,32 +47,32 @@ module Sm5 : SM5 =
 struct
 
   (* Refer to the document http://ropas.snu.ac.kr/~kwang/4190.310/17/hw5.pdf for
-     SM5's syntax, domain and semantic rules 
+     SM5's syntax, domain and semantic rules
   *)
 
   type loc = int * int
   type record = (string * loc) list
   type value = Z of int | B of bool | L of loc | Unit | R of record
-  type cmd = 
-    | PUSH of obj 
-    | POP 
-    | STORE 
-    | LOAD 
+  type cmd =
+    | PUSH of obj
+    | POP
+    | STORE
+    | LOAD
     | JTR of command * command
-    | MALLOC 
-    | BOX of int 
-    | UNBOX of string 
-    | BIND of string 
+    | MALLOC
+    | BOX of int
+    | UNBOX of string
+    | BIND of string
     | UNBIND
-    | GET 
-    | PUT 
-    | CALL 
-    | ADD 
-    | SUB 
-    | MUL 
-    | DIV 
-    | EQ 
-    | LESS 
+    | GET
+    | PUT
+    | CALL
+    | ADD
+    | SUB
+    | MUL
+    | DIV
+    | EQ
+    | LESS
     | NOT
   and obj = Val of value | Id of string | Fn of string * command
   and command = cmd list
@@ -93,25 +93,25 @@ struct
 
   let gc_mode = ref false
 
-  let loc_to_str (base, offset) = 
+  let loc_to_str (base, offset) =
     Printf.sprintf "<%d, %d>" base offset
 
-  let val_to_str = function 
+  let val_to_str = function
     | Z i -> string_of_int i
     | B b -> string_of_bool b
-    | R pair_list -> 
+    | R pair_list ->
       let pair_to_str (s, l) = Printf.sprintf "(%s, %s)" s (loc_to_str l) in
       "{" ^  (String.concat ", " (List.map pair_to_str pair_list)) ^ "}"
     | Unit -> "unit"
     | L l -> loc_to_str l
 
-  let rec cmd_to_str indent cmd = 
+  let rec cmd_to_str indent cmd =
     match cmd with
     | PUSH obj -> Printf.sprintf "push %s" (obj_to_str indent obj)
     | POP -> "pop"
     | STORE -> "store"
     | LOAD -> "load"
-    | JTR (c1, c2) -> 
+    | JTR (c1, c2) ->
       let c1_str = (command_to_str ("  " ^ indent) c1) in
       let c2_str = (command_to_str ("  " ^ indent) c2) in
       Printf.sprintf "jtr (\n%s,\n%s)" c1_str c2_str
@@ -130,18 +130,18 @@ struct
     | EQ -> "eq"
     | LESS -> "less"
     | NOT -> "not"
-  and obj_to_str indent obj = 
+  and obj_to_str indent obj =
     match obj with
     | Val v -> val_to_str v
     | Id x -> x
-    | Fn (f, comm) -> 
+    | Fn (f, comm) ->
       let new_indent = "  " ^ indent in
       let comm_str = command_to_str new_indent comm in
       Printf.sprintf "(%s,\n%s\n%s)" f comm_str new_indent
-  and command_to_str indent comm = 
-    indent ^ "[" ^ 
+  and command_to_str indent comm =
+    indent ^ "[" ^
     (String.concat (";\n" ^ indent) (List.map (cmd_to_str indent) comm)) ^ "]"
-  
+
   let rec proc_to_str indent (x, comm, env) =
     let new_indent = ("  " ^ indent) in
     let comm_str = command_to_str new_indent comm in
@@ -152,10 +152,10 @@ struct
     | Loc l -> loc_to_str l
     | Proc p -> proc_to_str indent p
   and env_to_str indent env =
-    let entry_to_str (x, ev) = 
+    let entry_to_str (x, ev) =
       Printf.sprintf "%s : %s" x (evalue_to_str indent ev)
     in
-    indent ^ "[" ^ 
+    indent ^ "[" ^
     (String.concat (";\n" ^ indent) (List.map entry_to_str env)) ^ "]"
 
   let sval_to_str = function
@@ -171,9 +171,9 @@ struct
       Printf.sprintf "%s : %s" (loc_to_str l) (val_to_str v)
     in
     String.concat "\n" (List.map entry_to_str m)
-  
-  let cont_to_str k = 
-    let entry_to_str (comm, env) = 
+
+  let cont_to_str k =
+    let entry_to_str (comm, env) =
       let comm_str = command_to_str "  " comm in
       let env_str = env_to_str "  " env in
       Printf.sprintf "Command :\n%s\nEnv :\n%s" comm_str env_str
@@ -183,16 +183,16 @@ struct
   let lookup_env x e =
     try List.assoc x e with Not_found -> raise (Error ("Unbound name : " ^ x))
 
-  let lookup_record x r = 
+  let lookup_record x r =
     try List.assoc x r with Not_found -> raise (Error ("Unfound field : " ^ x))
 
-  let load l m = 
-    try List.assoc l m with 
+  let load l m =
+    try List.assoc l m with
     Not_found -> raise (Error ("Uninitialized location : %s" ^ (loc_to_str l)))
 
-  let store l v m = 
-    if List.mem_assoc l m then 
-      (l, v) :: (List.remove_assoc l m) 
+  let store l v m =
+    if List.mem_assoc l m then
+      (l, v) :: (List.remove_assoc l m)
     else (l, v) :: m
 
   let mem_limit = 128
@@ -200,7 +200,7 @@ struct
   let loc_id = ref 0
 
   let reachable_locs : (loc list) ref = ref []
-  
+
   (* TODO : Complete this function.
    * Implement the GC algorithm introduced in class material.
    *)
@@ -211,8 +211,77 @@ struct
     else
       let _ = reachable_locs := [] in
       (* TODO : Add the code that marks the reachable locations.
-       * let _ = ... 
+       * let _ = ...
        *)
+       let baseL = ref [] in
+       let rec cSF : svalue -> unit = fun x -> (
+         match x with
+         | V a -> (
+           cVF a
+          )
+         | P (_, com, env) -> (
+           let _ = List.iter cCF com in
+           let (_, eee) = List.split env in
+           (List.iter cEF eee)
+          )
+         | M (a, b) -> (cEF b)
+         | _ -> ()
+        ) and
+        cVF : value -> unit = fun x -> (
+          match x with
+          | L (a, b) -> (
+            let _ = (baseL := !baseL @ [a]) in
+            try (let aaaa = List.assoc (a,b) m in cVF aaaa) with Not_found -> ()
+           )
+          | R ll -> (
+            let (all , bll) = (List.split ll) in
+            let fff x = (
+              let (a, b) = x in
+              let _ = (baseL := !baseL @ [a]) in
+              try (let aaaa = List.assoc (a,b) m in cVF aaaa) with Not_found -> ()
+              (* if not ((try List.assoc (a,b) m with Not_found -> ()) = ()) then (cVF (List.assoc (a,b) m)) else () *)
+            ) in
+            List.iter fff bll
+           )
+          | _ -> ()
+        ) and
+        cEF: evalue -> unit = fun x -> (
+          match x with
+          | Loc (aaq,bbq) -> cSF (V (L (aaq, bbq)))
+          | Proc (_, com, env) -> (
+            let _ = List.iter cCF com in
+            let (_, eee) = List.split env in
+            (List.iter cEF eee)
+          )
+        ) and
+        cCF : cmd -> unit = fun x -> (
+          match x with
+          | PUSH o -> (
+            match o with
+            | Fn (_, pp) -> (
+              List.iter cCF pp
+            )
+            | _ -> ()
+          )
+          | _ -> ()
+        ) and
+        cKF : (command * environment) -> unit = fun x -> (
+          let (cp, eeq) = x in
+          let (_,eq) = List.split eeq in
+          let _ = List.iter cCF cp in
+          List.iter cEF eq
+        ) in
+        let (_, eeee) = List.split e in
+        let _ = List.iter cSF s in
+        let _ = List.iter cEF eeee in
+        let _ = List.iter cCF c in
+        let _ = List.iter cKF k in
+        let concatF x = (
+          let (q, _) = x in
+          let (aaa, bbb) = q in
+          if (List.mem aaa !baseL) then (let _ = reachable_locs := !reachable_locs @ [(aaa,bbb)] in ()) else (())
+        ) in
+        let _ = List.iter concatF m in
       let new_m = List.filter (fun (l, _) -> List.mem l !reachable_locs) m in
       if List.length new_m < mem_limit then
         let _ = loc_id := !loc_id + 1 in
@@ -221,7 +290,7 @@ struct
       else
         raise GC_Failure
 
-  let malloc () = 
+  let malloc () =
     let _ = loc_id := !loc_id + 1 in
     (!loc_id, 0)
 
@@ -235,7 +304,7 @@ struct
     match v1, v2 with
     | Z z1, Z z2 -> z1 = z2
     | B b1, B b2 -> b1 = b2
-    | R r1, R r2 -> 
+    | R r1, R r2 ->
       List.sort Pervasives.compare r1 = List.sort Pervasives.compare r2
     | Unit, Unit -> true
     | L (base1, z1), L (base2, z2) ->
@@ -252,20 +321,20 @@ struct
     | (w :: s, m, e, POP :: c , k) -> (s, m, e, c, k)
     | (V (L l) :: V v :: s, m, e, STORE :: c, k) -> (s, store l v m, e, c, k)
     | (V (L l) :: s, m, e, LOAD :: c, k) -> (V (load l m) :: s, m, e, c, k)
-    | (V (B b)::s, m, e, JTR (c1,c2) :: c, k) -> 
+    | (V (B b)::s, m, e, JTR (c1,c2) :: c, k) ->
       (s, m, e, (if b then c1 @ c else (c2 @ c)), k)
-    | (s, m, e, MALLOC :: c, k) -> 
+    | (s, m, e, MALLOC :: c, k) ->
       if !gc_mode then
         let (new_l, new_m) = malloc_with_gc s m e c k in
         (V (L new_l) :: s, new_m, e, c, k)
-      else 
+      else
         (V (L (malloc ())) :: s, m, e, c, k)
     | (s, m, e, BOX z :: c, k) ->
-      if z <= 0 then 
-        raise (Error "Box(n): non-positive n") 
-      else 
+      if z <= 0 then
+        raise (Error "Box(n): non-positive n")
+      else
         (box_stack s z [], m, e, c, k)
-    | (V (R r) :: s, m, e, UNBOX x :: c, k) -> 
+    | (V (R r) :: s, m, e, UNBOX x :: c, k) ->
       (V (L (lookup_record x r)) :: s, m, e, c, k)
     | (V (L l) :: s, m, e, BIND x :: c, k) -> (s, m, (x, Loc l) :: e, c, k)
     | (P p :: s, m, e, BIND x :: c, k) -> (s, m, (x, Proc p) :: e, c, k)
@@ -274,40 +343,40 @@ struct
       (s, store l v m, (x, Loc l) :: e', c', (c, e) :: k)
     | (s, m, e, [], (c, e') :: k) -> (s, m, e', c, k)
     | (s, m, e, GET :: c, k) -> (V (Z (read_int())) :: s, m, e, c, k)
-    | (V (Z z) :: s, m, e, PUT :: c, k) -> 
+    | (V (Z z) :: s, m, e, PUT :: c, k) ->
       let _ = print_endline (string_of_int z) in
       (s, m, e, c, k)
     | (V (Z z2) :: V (Z z1) :: s, m, e, ADD :: c, k) ->
       (V (Z (z1 + z2))::s, m, e, c, k)
     | (V (Z z) :: V (L (base, offset)) :: s, m, e, ADD :: c, k)
-    | (V (L (base, offset)) :: V (Z z) :: s, m, e, ADD :: c, k) -> 
+    | (V (L (base, offset)) :: V (Z z) :: s, m, e, ADD :: c, k) ->
       if offset + z >= 0 then
-        (V (L (base, offset + z)) :: s, m, e, c, k) 
-      else 
+        (V (L (base, offset + z)) :: s, m, e, c, k)
+      else
         raise (Error "Negative loc offset spawned")
     | (V (Z z2) :: V (Z z1) :: s, m, e, SUB :: c, k) ->
       (V (Z (z1 - z2)) :: s, m, e, c, k)
-    | (V (Z z) :: V (L (base, offset)) :: s, m, e, SUB :: c, k) -> 
+    | (V (Z z) :: V (L (base, offset)) :: s, m, e, SUB :: c, k) ->
       if offset - z >= 0 then
-        (V (L (base, offset - z)) :: s, m, e, c, k) 
-      else  
+        (V (L (base, offset - z)) :: s, m, e, c, k)
+      else
         raise (Error "Negative loc offset spanwed")
-    | (V (Z z2) :: V (Z z1) :: s, m, e, MUL :: c, k) -> 
+    | (V (Z z2) :: V (Z z1) :: s, m, e, MUL :: c, k) ->
       (V (Z (z1 * z2)) :: s, m, e, c, k)
-    | (V (Z z2) :: V (Z z1) :: s, m, e, DIV :: c, k) -> 
+    | (V (Z z2) :: V (Z z1) :: s, m, e, DIV :: c, k) ->
       (V (Z (z1 / z2)) :: s, m, e, c, k)
-    | (V v2 :: V v1 :: s, m, e, EQ :: c, k) -> 
+    | (V v2 :: V v1 :: s, m, e, EQ :: c, k) ->
       (V (B (is_equal v1 v2)) :: s, m, e, c, k)
-    | (V (Z z2) :: V (Z z1) :: s, m, e, LESS :: c, k) -> 
+    | (V (Z z2) :: V (Z z1) :: s, m, e, LESS :: c, k) ->
       (V (B (z1 < z2)) :: s, m, e, c, k)
     | (V (B b) :: s, m, e, NOT :: c, k) -> (V (B (not b)) :: s, m, e, c, k)
     | s, m, e, c, k ->
       raise (Error "Invalid machine state")
 
-  let rec run_helper (s, m, e, c, k) = 
+  let rec run_helper (s, m, e, c, k) =
     match c, k with
     | [], [] -> ()
-    | _ -> 
+    | _ ->
       let _ = if !debug_mode then
         (print_endline "====== Machine state ======";
         print_newline();
@@ -329,6 +398,6 @@ struct
       in
       run_helper (step (s, m, e, c, k))
 
-  let run c = run_helper ([], [], [], c, []) 
+  let run c = run_helper ([], [], [], c, [])
 
 end
